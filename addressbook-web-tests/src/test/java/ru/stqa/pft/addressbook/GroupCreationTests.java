30dd23a43927a63cc9f97017d27aa6f0514e1b85
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-04-24 21:16:53 +0500

    Исправление имени переменной login

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -15,10 +15,10 @@ public class GroupCreationTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
-    Login("admin", "secret");
+    login("admin", "secret");
   }
 
-  private void Login(String username, String password) {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
