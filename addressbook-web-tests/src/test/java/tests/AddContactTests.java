5510520801c7bec5d59ab907e377af2a08badcf2
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-04-27 17:46:44 +0500

    Удаление обработки исключений throws Exception

--- addressbook-web-tests/src/test/java/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/tests/AddContactTests.java
@@ -6,7 +6,7 @@ import model.ContactData;
 public class AddContactTests extends TestBase{
 
   @Test
-  public void testAddContact() throws Exception {
+  public void testAddContact() {
     app.getNavigationHelper().goToAddNewPage();
     app.getContactHelper().fillContact(new ContactData("Name", "MiddleName", "Last", "Nick", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "e-mail"));
     app.getContactHelper().submitContact();
--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -6,7 +6,7 @@ import model.GroupData;
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test", "test"));
