e50f91209368a4f93aa19a1645e34d17614c0682
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-04-27 17:41:36 +0500

    Переименование Contact в AddContact

--- addressbook-web-tests/src/test/java/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/tests/AddContactTests.java
@@ -3,10 +3,10 @@ package tests;
 import org.testng.annotations.Test;
 import model.ContactData;
 
-public class ContactTests extends TestBase{
+public class AddContactTests extends TestBase{
 
   @Test
-  public void testContact() throws Exception {
+  public void testAddContact() throws Exception {
     app.getNavigationHelper().goToAddNewPage();
     app.getContactHelper().fillContact(new ContactData("Name", "MiddleName", "Last", "Nick", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "e-mail"));
     app.getContactHelper().submitContact();
