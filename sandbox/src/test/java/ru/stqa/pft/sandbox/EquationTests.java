c5a731b33182a17392efe9655fc06c97143fce6d
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-03 19:43:14 +0500

    Свёртка вложенных проверок

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -21,4 +21,23 @@ public class EquationTests {
     Equation e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2);
   }
+
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConst() {
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
+
 }
