7d73d3693315cd5e0af402693e8032c6eb371c15
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-03 19:16:21 +0500

    Проверка условий с If-Then-Else

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PointTest {
+public class PointTests {
   @Test
   public void testDistance1() {
     Point p1 = new Point(-2, 4);
