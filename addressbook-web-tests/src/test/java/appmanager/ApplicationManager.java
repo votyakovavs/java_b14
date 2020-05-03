9a62eb0540020621cd5dd983103239df8be82c9e
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-03 21:22:44 +0500

    Запуск в разных браузерах

--- addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,16 +15,26 @@ public class ApplicationManager {
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private SessionHelper sessionHelper;
+  private final String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper = new SessionHelper(wd);
+    SessionHelper sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
 
@@ -30,9 +43,11 @@ public class ApplicationManager {
     wd.findElement(By.name("user"));
     wd.quit();
   }
+
   public void allert() {
     wd.switchTo().alert().accept();
   }
+
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -1,20 +1,21 @@
 package tests;
 
 import appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp() {
     app.init();
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void tearDown() {
     app.stop();
   }
 }
