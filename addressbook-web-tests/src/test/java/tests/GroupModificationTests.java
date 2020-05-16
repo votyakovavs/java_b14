f199f1bef8784a6cacc884c116439a376a5e8c5d
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-17 00:30:43 +0500

    Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test2", null, null));
     }
     int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test", "test"));
     app.getGroupHelper().submitGroupModification();
