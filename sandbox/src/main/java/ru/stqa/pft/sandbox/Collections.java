6861882eca83907400689ed95750182ad37b2e83
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-16 19:22:14 +0500

    Коллекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    /*for (int i = 0; i < langs.length; i++) {
+      System.out.println("Я хочу выучить " + langs[i]);
+    }*/
+
+    for (String l : langs) {
+      System.out.println("Я хочу выучить " + l);
+    }
+    
+    /*List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    languages.add("PHP");*/
+
+    List/*<String>*/ languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (/*String*/ Object l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+
+    for (int i = 0; i < languages.size(); i++) {
+      System.out.println("Я хочу выучить " + languages.get(i));
+    }
+
+  }
+
+}
