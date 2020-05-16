3051acf7f0d8888af8d460e33d8fb00dc3404ea4
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-17 01:19:29 +0500

    Сравнение коллекций

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -16,11 +16,13 @@ public class GroupDeletionTests extends TestBase {
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     // int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before.size()-1);
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     // int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size() - 1);
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
