e375f494d7cbc68174b70a7aa44c51c283650e4c
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-05-17 15:25:46 +0500

    Сортировка списков

--- addressbook-web-tests/src/test/java/model/GroupData.java
+++ addressbook-web-tests/src/test/java/model/GroupData.java
@@ -10,8 +10,9 @@ public class GroupData {
   private final String header;
   private final String footer;
 
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -57,12 +58,11 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
-            Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    return Objects.hash(name);
   }
 }
