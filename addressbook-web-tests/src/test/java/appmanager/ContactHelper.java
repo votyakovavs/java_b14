c718a9309c7db3ebac053b1e553c3e61a7dcc7bf
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-06-20 13:05:51 +0500

    Причесывание кода

--- addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
@@ -51,16 +51,6 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//input[@value='Delete']"));
   }
 
-  /*public void longInitEditContactById(int id) {
-    WebElement checkbox = wd.findElement(By.cssSelector(String.format("//input[@value='%s']", id)));
-    WebElement row = checkbox.findElement(By.xpath("./../.."));
-    List<WebElement> cells = row.findElements(By.tagName("td"));
-    cells.get(7).findElement(By.tagName("a")).click();
-
-    wd.findElement(By.xpath(String.format("//input[@value='%s']/../../td[8]/a", id))).click();
-    wd.findElement(By.xpath(String.format(".//tr[input[@value='%s']]/td[8]/a", id))).click();
-  }*/
-
   public void initEditContactById(int id) {
     wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s'", id))).click();
   }
