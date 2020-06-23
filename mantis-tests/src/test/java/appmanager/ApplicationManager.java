3c361c506cbe8bc007f2595b98e6406b6b837d20
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-06-23 16:40:40 +0500

    Подготовка проекта для тестирования MantisBT

---
+++ mantis-tests/src/test/java/appmanager/ApplicationManager.java
@@ -0,0 +1,47 @@
+package appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  private final String browser;
+  private final Properties properties;
+  WebDriver wd;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.name("user"));
+    wd.quit();
+  }
+
+}
---
+++ mantis-tests/src/test/java/tests/TestBase.java
@@ -0,0 +1,27 @@
+package tests;
+
+import appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws IOException {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+
+}
