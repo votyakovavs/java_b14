a770d2921547e7e0117dce11347748e7ef37df50
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-06-25 02:45:10 +0500

    REST взаимодействие с GitHub

---
+++ github-sample/src/test/java/github/GithubTests.java
@@ -0,0 +1,20 @@
+package github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("ef960f1b900a98bb9ba82a544675313410a52592");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("votyakovavs", "java_b14")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+
+}
