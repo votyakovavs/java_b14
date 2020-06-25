0d65bbc97b354bae0837967915586543e7151a76
votyakovavs <63795252+votyakovavs@users.noreply.github.com>
2020-06-25 13:42:43 +0500

    Испрочен тест Square

--- github-sample/src/test/java/github/GithubTests.java
+++ github-sample/src/test/java/github/GithubTests.java
@@ -10,7 +10,7 @@ public class GithubTests {
 
   @Test
   public void testCommits() throws IOException {
-    Github github = new RtGithub("ef960f1b900a98bb9ba82a544675313410a52592");
+    Github github = new RtGithub("b0ff7047fac5739ca2f35328e74ef5f9c18a817a");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("votyakovavs", "java_b14")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
       System.out.println(new RepoCommit.Smart(commit).message());
